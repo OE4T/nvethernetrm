12bdb2f1ea6008d7c4d688f5eaeb06c2f7675f49
Bhadram Varka <vbhadram@nvidia.com>
2024-06-05 15:02:11 +0000

    osi: core: WAR Werror=frame-larger-than and ivc issue
    Issue:
    1. compilation issue:
    error: the frame size of 3232 bytes is larger than 2048 bytes
    [-Werror=frame-larger-than=]
    
    Because of increase in the value of OSI_MACSEC_SC_INDEX_MAX
    it resulted in increase in the size of ivc_msg_common
    more than 2048 bytes.
    
    2. IVC communication breaking because IVC buffer size
    increased more than 2048 because of #1.
    
    WAR: Revert back to the orin value for OSI_MACSEC_SC_INDEX_MAX
    
    Bug 4687787
    
    Change-Id: I5262cd772acf6b4275cc3f47696f0072b70916e0
    Signed-off-by: Bhadram Varka <vbhadram@nvidia.com>

--- include/osi_common.h
+++ include/osi_common.h
@@ -231,7 +231,7 @@
 /**
  * @brief Maximum number of Secure Channels supported
  */
-#define OSI_MACSEC_SC_INDEX_MAX		48
+#define OSI_MACSEC_SC_INDEX_MAX		16
 
 #ifndef OSI_STRIPPED_LIB
 /* HW supports 8 Hash table regs, but eqos_validate_core_regs only checks 4 */
