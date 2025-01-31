13f613008ab7681dd8db229eb764ba80883e4e6d
Mohan Thadikamalla <mohant@nvidia.com>
2025-01-31 04:50:28 +0000

    common: Exclude internal functions from ICDs
    Issue:
    Internal functions were included
    in the NVETHERNETRM_PIF and NVETHERNETCL_PIF
    ICD Doxygen compilation.
    
    Fix:
    Exclude internal function documentation
    from NVETHERNETRM_PIF and NVETHERNETCL_PIF ICDs
    
    Jira NET-2169
    Jira NET-2170
    
    Change-Id: I62ee0ff32efe0c9698fd32ce36ee35da408b40c0
    Signed-off-by: Mohan Thadikamalla <mohant@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/kernel/nvethernetrm/+/3294123
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>
    Reviewed-by: Bhadram Varka <vbhadram@nvidia.com>
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: Ashutosh Jha <ajha@nvidia.com>

--- include/osi_common.h
+++ include/osi_common.h
@@ -322,6 +322,7 @@
 #endif /* OSI_DEBUG */
 /** @} */
 
+/** \cond DO_NOT_DOCUMENT */
 /**
  * @brief osi_memset - osi memset
  *
@@ -344,6 +345,7 @@ static inline void osi_memset(void *s, nveu8_t c, nveu64_t count)
 		xs[i] = c;
 	}
 }
+/** \endcond */
 
 /**
  * @brief unused function attribute
@@ -393,6 +395,7 @@ struct osi_pdma_vdma_data {
 	nveu32_t vdma_chans[OSI_MGBE_MAX_NUM_CHANS];
 };
 
+/** \cond DO_NOT_DOCUMENT */
 /**
  * @brief osi_valid_pbl_value - returns the allowed pbl value.
  * @note
@@ -433,6 +436,7 @@ static inline nveu32_t osi_valid_pbl_value(nveu32_t pbl_value)
 
 	return allowed_pbl;
 }
+/** \endcond */
 
 /**
  * @addtogroup PPS related information
